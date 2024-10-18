d6231ab05de7370eab987ba68f40bcef2499ce64
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-18 17:42:13 +0200

    findfilament.cmake: more libs are required

--- 3rdparty/cmake/Findfilament.cmake
+++ 3rdparty/cmake/Findfilament.cmake
@@ -2,12 +2,19 @@ find_path(filament_INCLUDE_DIR NAMES filament/FilamentAPI.h)
 find_library(filament_LIBRARY NAMES filament PATH_SUFFIXES x86_64)
 
 if(filament_INCLUDE_DIR AND filament_LIBRARY)
-    add_library(filament::filament UNKNOWN IMPORTED)
-    set_target_properties(
-        filament::filament
-        PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${filament_INCLUDE_DIR}"
-            IMPORTED_LOCATION "${filament_LIBRARY}"
+    add_library(filament::filament INTERFACE IMPORTED)
+    target_include_directories(filament::filament INTERFACE ${filament_INCLUDE_DIR})
+    target_link_libraries(filament::filament INTERFACE ${filament_LIBRARY})
+
+    find_library(filament_utils NAMES utils PATH_SUFFIXES x86_64)
+    find_library(filament_filabridge NAMES filabridge PATH_SUFFIXES x86_64)
+    find_library(filament_backend NAMES backend PATH_SUFFIXES x86_64)
+    find_library(filament_geometry NAMES geometry PATH_SUFFIXES x86_64)
+    target_link_libraries(filament::filament INTERFACE
+        ${filament_utils}
+        ${filament_filabridge}
+        ${filament_backend}
+        ${filament_geometry}
     )
 endif()
 
